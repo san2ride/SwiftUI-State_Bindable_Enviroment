f1e51ec217c2f3da6ffca843ecf979986961e5c4
Jason <sanchezjason@me.com>
2024-02-15 18:47:07 -0700

    swiftui/state_bindable_enviroment- Project setup

---
+++ SwiftUI_iOS17/SwiftUI_iOS17/SwiftUI_iOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_iOS17App.swift
+//  SwiftUI_iOS17
+//
+//  Created by don't touch me on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_iOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
